892c27e4f8a4a1a0f2932f9a0b5170889137e079
ryanpodonnell1 <48141361+ryanpodonnell1@users.noreply.github.com>
2021-06-01 15:11:29 -0400

    Update reset.sh

--- reset.sh
+++ reset.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-docker build . -t 611181293678.dkr.ecr.us-east-1.amazonaws.com/ryantest:validator
-docker push 611181293678.dkr.ecr.us-east-1.amazonaws.com/ryantest:validator
+docker build . -t <account_id>.dkr.ecr.us-east-1.amazonaws.com/ryantest:validator
+docker push <account_id>.dkr.ecr.us-east-1.amazonaws.com/ryantest:validator
 kubectl delete -f validatingwebhook.yml
-kubectl delete pods --all -n tgrccloudsecurity
+kubectl delete pods --all -n $NAMESPACE
 sleep 5
 kubectl apply -f validatingwebhook.yml
-kubectl run testpod --image=nginx -n tgrccloudsecurity
-kubectl get pods -n tgrccloudsecurity
+kubectl run testpod --image=nginx -n $NAMESPACE
+kubectl get pods -n $NAMESPACE
 
